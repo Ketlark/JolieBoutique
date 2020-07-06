e879880862499604b89c5a1884ddcb749d58cd56
Sébastien Kerroué <sebastien.kerroue@efrei.net>
2020-07-06 13:42:44 +0200

    Adding profile page

--- front/src/pages/Account.vue
+++ front/src/pages/Account.vue
@@ -1,6 +1,16 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>Account</h1>
+  <q-page class="flex">
+    <div class="container margin-top">
+      <div class="row justify-content-center" style="margin-bottom:200px; margin-top:80px;">
+        <div class="col-10 text-center">
+          <p><b>Prénom nom</b><br>adresse@email.com</p>
+        </div>
+      </div>
+      <div class="row justify-content-center" style="margin-top:10px;">
+        <button class="edit-profile-button text-center col-10 mb-3" v-on:click="addToCart()"><p>Modifier mon profil</p></button>
+        <button class="show-parcels-button text-center col-10"><p>Mes commandes</p></button>
+      </div>
+    </div>
   </q-page>
 </template>
 
